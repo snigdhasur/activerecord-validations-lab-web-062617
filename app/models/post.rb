ba7076cd59ab00da0cdf93d867742c0516cd817f
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-07-24 17:57:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
+
 class Post < ActiveRecord::Base
-end
+	validates :title, presence: true, length: {minimum: 10}
+	validates :content, presence: true, length: {minimum: 250}
+	validates :summary, presence: true, length: {maximum: 250}
+	validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+
+	validates_each :title do |post, attr, value|
+		 post.errors.add(attr, 'must be clickbait') if value !~ /Won't Believe/ || /Secret/ || /Top \d/ || /Guess/
+	end
+
+end 
